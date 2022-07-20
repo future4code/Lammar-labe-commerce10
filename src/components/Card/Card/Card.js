c37b70a0458f09a0652e5af7113fc9f27e62b505
Caroline Marques <carolinemcs@hotmail.com>
2022-07-19 23:15:16 -0300

    Iniciando o home

---
+++ src/components/Card/Card/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Card = () => {
+
+    return (
+
+        <main>
+            <section>
+                <div className="card">
+                     <img className="imagem" src="" alt=""></img>
+                     <h3 className="nome">Brinquedo</h3>
+                     <h3 className="valor">R$</h3>
+                     <button>Adicionar</button>
+                 </div>
+                
+            </section>
+        </main>
+      
+    )     
+}
+
+export default Card;
